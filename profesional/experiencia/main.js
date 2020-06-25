1582885ab6a281b9f48237732940fe78bdb823ea
Joaquin <xvitkleo@gmail.com>
2020-06-25 06:02:48 -0500

    terminado

---
+++ profesional/experiencia/main.js
@@ -0,0 +1,4 @@
+document.querySelector('.boton.crear-experiencia_btn').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('.modal-crear').style.display = 'flex';
+})
---
+++ profesional/formacion/main.js
@@ -0,0 +1,4 @@
+document.querySelector('.boton.crear-formacion_btn').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('.modal-crear').style.display = 'flex';
+})
