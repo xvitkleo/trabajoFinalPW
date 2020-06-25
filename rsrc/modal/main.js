9128d216ce09c80e79ecccb9705537d3c155c669
Joaquin <xvitkleo@gmail.com>
2020-06-25 17:14:09 -0500

    terminado

--- rsrc/modal/main.js
+++ rsrc/modal/main.js
@@ -1,7 +1,8 @@
 
-document.querySelector('.close-modal').addEventListener('click', () => closeModal());
-document.querySelector('.crear').addEventListener('click', () => closeModal());
+document.querySelector('.close-modal').addEventListener('click', () => closeModal(true));
+document.querySelector('.crear').addEventListener('click', () => closeModal(false));
 
-function closeModal() {
-    document.querySelector('.modal-crear').style.display = 'none';
+function closeModal(closeModal) {
+    if(closeModal) document.querySelector('.modal-crear').style.display = 'none';
+    else if(document.querySelector('.modal_form').checkValidity()) document.querySelector('.modal-crear').style.display = 'none';
 }
